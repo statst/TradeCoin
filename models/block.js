cf5c096f5132a873ba0f3057c5c850b0733cc181
jyoti <jyoti@jyotis-MacBook-Pro-2.local>
2020-06-06 01:40:07 -0400

    addedmineBlock with tests

--- models/block.js
+++ models/block.js
@@ -4,7 +4,17 @@ class Block{
         this.lastHash = lastHash;
         this.hash = hash;
         this.data = data;
-    }   
+    } 
+    static genesis(){
+        return new this(GENESIS_DATA);
+    }  
+    static mineBlock({ lastBlock, data }) {
+    return new this({
+        timestamp: Date.now(),
+        lastHash: lastBlock.hash, data
+        
+    })
+}
 }
 
 module.exports = Block;
--- models/block.test.js
+++ models/block.test.js
@@ -1,35 +1,52 @@
 const Block = require('./block');
-const {GENESIS_DATA} = require('../config')
+const {GENESIS_DATA} = require('./config')
 
-describe('Block', ()=>{
-    const timestamp = 'any-date';
-    const lastHash = 'last hash value';
-    const hash = 'hash value';
-    const data = ['first transaction', 'data'];
-    const block = new Block({
-			timestamp,
-			lastHash,
-			hash,
-			data,
-		}); 
+describe('Block', () => {
+	const timestamp = 'any-date';
+	const lastHash = 'last hash value';
+	const hash = 'hash value';
+	const data = ['first transaction', 'data'];
+	const block = new Block({ timestamp, lastHash, hash, data });
 
-    it('has a timestamp, lastHash, hash and data', () => {
-         expect(block.timestamp).toEqual(timestamp);
-		 expect(block.lastHash).toEqual(lastHash);
-		 expect(block.hash).toEqual(hash);
-		 expect(block.data).toEqual(data);
-    });
+	it('has a timestamp, lastHash, hash and data property', () => {
+		expect(block.timestamp).toEqual(timestamp);
+		expect(block.lastHash).toEqual(lastHash);
+		expect(block.hash).toEqual(hash);
+		expect(block.data).toEqual(data);
+	});
 
-describe('genesis()', () =>{
-    const genesisBlock = Block.genesis();
+	describe('genesis()', () => {
+		const genesisBlock = Block.genesis();
 
-    it ('returns a block instance', () =>{
-        expect(genesisBlock instanceof Block).toBe(true);
-    })
+		console.log('genesisBlock', genesisBlock);
 
-    it('returns the genesis data', () => {
-        expect(genesisBlock).toEqual(GENESIS_DATA);
+		it('returns a block instance', () => {
+			expect(genesisBlock instanceof Block).toBe(true);
+		});
 
-    })
-})
+		it('returns the genesis data', () => {
+			expect(genesisBlock).toEqual(GENESIS_DATA);
+		});
+	});
+	describe('mineBlock()', () => {
+		const lastBlock = Block.genesis();
+		const data = 'mined data';
+		const minedBlock = Block.mineBlock({ lastBlock, data });
+
+		it('returns a Block instance', () => {
+			expect(minedBlock instanceof Block).toBe(true);
+		});
+
+		it('sets the `lastHash` to be the `hash` of the lastBlock', () => {
+			expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+		});
+
+		it('sets the `data`', () => {
+			expect(minedBlock.data).toEqual(data);
+		});
+
+		it('sets a `timestamp`', () => {
+			expect(minedBlock.timestamp).not.toEqual(undefined);
+		});
+	});
 });
