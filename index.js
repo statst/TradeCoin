1f4b27aeee3404ab8fe40848bf6e94c3ce44ed45
jyoti <jshindems@gmail.com>
2020-09-10 01:35:00 -0400

    synchronizing chain on connect

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const request = require('request');
 const Blockchain = require('./models/blockchain');
 const PubSub = require('./pubsub')
 
 const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
+
+const DEFAULT_PORT = 3001;
+const ROOT_NODE_ADDRESS = `http:localhost:${DEFAULT_PORT}`;
 
 setTimeout (() => pubsub.broadcastChain(), 1000);
 
@@ -18,11 +22,30 @@ app.get('/api/blocks', (req, res) =>{
 app.post('/api/mine', (req, res) => {
     const { data } =req.body;
     blockchain.addBlock({ data });
+    pubsub.broadcastChain();
     res.redirect('/api/blocks');
 })
 
+const syncChains = () =>{
+    request({url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) =>{
+        if(!error && response.statusCode === 200) {
+            const rootChain = JSON.parse(body);
+            console.log('replace chain on a sync with', rootChain);
+            blockchain.replaceChain(rootChain);
+        }
+    });
+}
 
-const PORT = 8000;
+let PEER_PORT;
+
+if(process.env.GENERATE_PEER_PORT === 'true'){
+    PEER_PORT = DEFAULT_PORT + 
+    Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
-    console.log(`App is listening on localhost ${PORT}`)
+    console.log(`App is listening on localhost ${PORT}`);
+
+    syncChains();
 })
