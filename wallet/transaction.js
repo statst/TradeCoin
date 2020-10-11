0aa91a4f9f777488ae29672af6ad910b7aa1815c
jyoti <jshindems@gmail.com>
2020-10-11 19:39:17 -0400

    added reward transaction mining functionality and tested it

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,9 +1,10 @@
 const uuid = require('uuid/v1');
 const { verifySignature } = require('../util');
+const { REWARD_INPUT, MINING_INPUT, MINING_REWARD } = require('../config')
 class Transaction {
 	constructor({ senderWallet, recipient, amount, outputMap, input }) {
 		this.id = uuid();
-		this.outputMap = outputMap || this.createOutputMap({ senderWallet, recipient, amount });
+		this.outputMap = outputMap || this.createOutputMap({ senderWallet, recipient, amount, outputMap, input });
 		this.input = input || this.createInput({ senderWallet, outputMap: this.outputMap });
 	}
 	createOutputMap({ senderWallet, recipient, amount }) {
@@ -56,6 +57,13 @@ class Transaction {
 			return false;
 		}
 		return true;
+	}
+
+	static rewardTransaction({ minerWallet }){
+		return new this({
+			input: REWARD_INPUT,
+			outputMap: { [minerWallet.publicKey]: MINING_REWARD }
+		});
 	}
 }
 
